7cdcfead34ad941aa132456a1f72faba4c7142c9
bmpark <bmpark@jinhak.com>
2023-09-06 16:00:56 +0900

    rollup 설정

--- src/changeValueNotation/index.test.js
+++ src/changeValueNotation/index.test.js
@@ -1,73 +1,64 @@
-const assert = require("assert");
-const { changeValueNotation } = require("./index");
-describe("표기법 변환 함수 string", () => {
-    it("string, camel to camel", () => {
-        const result = changeValueNotation("notaionTest", "camel");
-        assert.equal(result, "notaionTest");
+const assert = require('assert');
+const { changeValueNotation } = require('./index');
+describe('표기법 변환 함수 string', () => {
+    it('string, camel to camel', () => {
+        const result = changeValueNotation('notaionTest', 'camel');
+        assert.equal(result, 'notaionTest');
     });
-    it("string, snake to camel", () => {
-        const result = changeValueNotation("notaion_test", "camel");
-        assert.equal(result, "notaionTest");
+    it('string, snake to camel', () => {
+        const result = changeValueNotation('notaion_test', 'camel');
+        assert.equal(result, 'notaionTest');
     });
-    it("string, pascal to camel", () => {
-        const result = changeValueNotation("NotaionTest", "camel");
-        assert.equal(result, "notaionTest");
-    });
-
-    it("string, camel to pascal", () => {
-        const result = changeValueNotation("notaionTest", "pascal");
-        assert.equal(result, "NotaionTest");
-    });
-    it("string, snake to pascal", () => {
-        const result = changeValueNotation("notaion_test", "pascal");
-        assert.equal(result, "NotaionTest");
-    });
-    it("string, pascal to pascal", () => {
-        const result = changeValueNotation("NotaionTest", "pascal");
-        assert.equal(result, "NotaionTest");
+    it('string, pascal to camel', () => {
+        const result = changeValueNotation('NotaionTest', 'camel');
+        assert.equal(result, 'notaionTest');
     });
 
-    it("string, camel to snake", () => {
-        const result = changeValueNotation("notaionTest", "snake");
-        assert.equal(result, "notaion_test");
+    it('string, camel to pascal', () => {
+        const result = changeValueNotation('notaionTest', 'pascal');
+        assert.equal(result, 'NotaionTest');
     });
-    it("string, snake to snake", () => {
-        const result = changeValueNotation("notaion_test", "snake");
-        assert.equal(result, "notaion_test");
+    it('string, snake to pascal', () => {
+        const result = changeValueNotation('notaion_test', 'pascal');
+        assert.equal(result, 'NotaionTest');
     });
-    it("string, pascal to snake", () => {
-        const result = changeValueNotation("NotaionTest", "snake");
-        assert.equal(result, "notaion_test");
+    it('string, pascal to pascal', () => {
+        const result = changeValueNotation('NotaionTest', 'pascal');
+        assert.equal(result, 'NotaionTest');
+    });
+
+    it('string, camel to snake', () => {
+        const result = changeValueNotation('notaionTest', 'snake');
+        assert.equal(result, 'notaion_test');
+    });
+    it('string, snake to snake', () => {
+        const result = changeValueNotation('notaion_test', 'snake');
+        assert.equal(result, 'notaion_test');
+    });
+    it('string, pascal to snake', () => {
+        const result = changeValueNotation('NotaionTest', 'snake');
+        assert.equal(result, 'notaion_test');
     });
 });
-describe("표기법 변환 함수 object", () => {
-    it("object, camel to camel", () => {
-        const result = changeValueNotation(
-            { notaionTest1: 1, notaionTest2: 2, notaionTest3: 3 },
-            "camel"
-        );
+describe('표기법 변환 함수 object', () => {
+    it('object, camel to camel', () => {
+        const result = changeValueNotation({ notaionTest1: 1, notaionTest2: 2, notaionTest3: 3 }, 'camel');
         assert.deepEqual(result, {
             notaionTest1: 1,
             notaionTest2: 2,
             notaionTest3: 3,
         });
     });
-    it("object, snake to camel", () => {
-        const result = changeValueNotation(
-            { notaionTest1: 1, notaion_test2: 2, notaion_test3: 3 },
-            "camel"
-        );
+    it('object, snake to camel', () => {
+        const result = changeValueNotation({ notaionTest1: 1, notaion_test2: 2, notaion_test3: 3 }, 'camel');
         assert.deepEqual(result, {
             notaionTest1: 1,
             notaionTest2: 2,
             notaionTest3: 3,
         });
     });
-    it("object, pascal to camel", () => {
-        const result = changeValueNotation(
-            { NotaionTest1: 1, NotaionTest2: 2, notaion_test3: 3 },
-            "camel"
-        );
+    it('object, pascal to camel', () => {
+        const result = changeValueNotation({ NotaionTest1: 1, NotaionTest2: 2, notaion_test3: 3 }, 'camel');
         assert.deepEqual(result, {
             notaionTest1: 1,
             notaionTest2: 2,
@@ -75,33 +66,24 @@ describe("표기법 변환 함수 object", () => {
         });
     });
 
-    it("object, camel to pascal", () => {
-        const result = changeValueNotation(
-            { notaionTest1: 1, notaionTest2: 2, notaionTest3: 3 },
-            "pascal"
-        );
+    it('object, camel to pascal', () => {
+        const result = changeValueNotation({ notaionTest1: 1, notaionTest2: 2, notaionTest3: 3 }, 'pascal');
         assert.deepEqual(result, {
             NotaionTest1: 1,
             NotaionTest2: 2,
             NotaionTest3: 3,
         });
     });
-    it("object, snake to pascal", () => {
-        const result = changeValueNotation(
-            { notaionTest1: 1, notaion_test2: 2, notaion_test3: 3 },
-            "pascal"
-        );
+    it('object, snake to pascal', () => {
+        const result = changeValueNotation({ notaionTest1: 1, notaion_test2: 2, notaion_test3: 3 }, 'pascal');
         assert.deepEqual(result, {
             NotaionTest1: 1,
             NotaionTest2: 2,
             NotaionTest3: 3,
         });
     });
-    it("object, pascal to pascal", () => {
-        const result = changeValueNotation(
-            { NotaionTest1: 1, NotaionTest2: 2, notaion_test3: 3 },
-            "pascal"
-        );
+    it('object, pascal to pascal', () => {
+        const result = changeValueNotation({ NotaionTest1: 1, NotaionTest2: 2, notaion_test3: 3 }, 'pascal');
         assert.deepEqual(result, {
             NotaionTest1: 1,
             NotaionTest2: 2,
@@ -109,33 +91,24 @@ describe("표기법 변환 함수 object", () => {
         });
     });
 
-    it("object, camel to snake", () => {
-        const result = changeValueNotation(
-            { notaionTest1: 1, notaionTest2: 2, notaionTest3: 3 },
-            "snake"
-        );
+    it('object, camel to snake', () => {
+        const result = changeValueNotation({ notaionTest1: 1, notaionTest2: 2, notaionTest3: 3 }, 'snake');
         assert.deepEqual(result, {
             notaion_test1: 1,
             notaion_test2: 2,
             notaion_test3: 3,
         });
     });
-    it("object, snake to snake", () => {
-        const result = changeValueNotation(
-            { notaionTest1: 1, notaion_test2: 2, notaion_test3: 3 },
-            "snake"
-        );
+    it('object, snake to snake', () => {
+        const result = changeValueNotation({ notaionTest1: 1, notaion_test2: 2, notaion_test3: 3 }, 'snake');
         assert.deepEqual(result, {
             notaion_test1: 1,
             notaion_test2: 2,
             notaion_test3: 3,
         });
     });
-    it("object, pascal to snake", () => {
-        const result = changeValueNotation(
-            { NotaionTest1: 1, NotaionTest2: 2, notaion_test3: 3 },
-            "snake"
-        );
+    it('object, pascal to snake', () => {
+        const result = changeValueNotation({ NotaionTest1: 1, NotaionTest2: 2, notaion_test3: 3 }, 'snake');
         assert.deepEqual(result, {
             notaion_test1: 1,
             notaion_test2: 2,
--- src/masking/index.js
+++ src/masking/index.js
@@ -1,34 +1,29 @@
 export const masking = (target, maskingNum = 3, maskingStart = 0) => {
-    if (typeof target !== "string") {
-        console.error("only string type");
-        return "";
+    if (typeof target !== 'string') {
+        console.error('only string type');
+        return '';
     }
-    if (
-        maskingNum < 0 ||
-        maskingStart < 0 ||
-        !Number.isInteger(maskingNum) ||
-        !Number.isInteger(maskingStart)
-    ) {
-        console.error("maskingNum and maskingStart are only positive integer");
+    if (maskingNum < 0 || maskingStart < 0 || !Number.isInteger(maskingNum) || !Number.isInteger(maskingStart)) {
+        console.error('maskingNum and maskingStart are only positive integer');
         return target;
     }
-    let masking = "*".repeat(maskingNum);
+    let masking = '*'.repeat(maskingNum);
     if (target.length < maskingNum) {
-        console.warn("maskingNum longer than target length");
-        masking = "*".repeat(target.length);
+        console.warn('maskingNum longer than target length');
+        masking = '*'.repeat(target.length);
         return masking;
     }
     if (target.length <= maskingStart) {
-        console.warn("maskingStart longer than target length");
+        console.warn('maskingStart longer than target length');
         return target;
     }
 
     if (target.length < maskingStart + maskingNum) {
-        console.log("!!");
+        console.log('!!');
         maskingNum = target.length - maskingStart;
     }
-    console.log("maskigNumb", maskingNum);
-    masking = "*".repeat(maskingNum);
+    console.log('maskigNumb', maskingNum);
+    masking = '*'.repeat(maskingNum);
 
     const left = target.substring(0, maskingStart);
     const right = target.substring(maskingStart + maskingNum);
@@ -38,4 +33,3 @@ export const masking = (target, maskingNum = 3, maskingStart = 0) => {
     // }
     return left + masking + right;
 };
-
